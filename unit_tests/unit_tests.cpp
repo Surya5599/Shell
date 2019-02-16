573d133777392be40144fd81262dc96e0acaec30
ssing072 <ssing072@ucr.edu>
2019-02-16 00:46:31 -0800

    Finished unit_testing

--- unit_tests/unit_tests.cpp
+++ unit_tests/unit_tests.cpp
@@ -2,7 +2,6 @@
 #include "Command.h"
 #include "singleCommand.h"
 #include "multipleCommand.h"
-#include <sstream>
 
 using namespace std;
 
@@ -19,17 +18,35 @@ TEST(SingleCommandTest, LS){
     
     singleCommand* s2 = new singleCommand("ls");
     s2->Parse();
+    testing::internal::CaptureStdout();
     s2->runCommand();
-
+    string output = testing::internal::GetCapturedStdout();
+    string expectedOutput = "bin\nCMakeCache.txt\nCMakeFiles\ncmake_install.cmake\nCMakeLists.txt\ngoogletest\nimages\nintegration_tests\nlib\nMakefile\nREADME.md\nrshell\nsrc\ntest\nunit_tests\n";
+    EXPECT_EQ(expectedOutput, output);
 
 }
 
 
-TEST(SingleCommandTest, MKDIR){
+TEST(SingleCommandTest, MKDIRandRM){
     
     singleCommand* s3 = new singleCommand("mkdir new");
     s3->Parse();
     s3->runCommand();
+    singleCommand* s2 = new singleCommand("ls");
+    s2->Parse();
+    testing::internal::CaptureStdout();
+    s2->runCommand();
+    string output = testing::internal::GetCapturedStdout();
+    string expectedOutput = "bin\nCMakeCache.txt\nCMakeFiles\ncmake_install.cmake\nCMakeLists.txt\ngoogletest\nimages\nintegration_tests\nlib\nMakefile\nnew\nREADME.md\nrshell\nsrc\ntest\nunit_tests\n";
+    EXPECT_EQ(expectedOutput, output);
+    singleCommand* s4 = new singleCommand("rm -rf new");
+    s4->Parse();
+    s4->runCommand();
+    testing::internal::CaptureStdout();
+    s2->runCommand();
+    string output2 = testing::internal::GetCapturedStdout();
+    string expectedOutput2 = "bin\nCMakeCache.txt\nCMakeFiles\ncmake_install.cmake\nCMakeLists.txt\ngoogletest\nimages\nintegration_tests\nlib\nMakefile\nREADME.md\nrshell\nsrc\ntest\nunit_tests\n";
+    EXPECT_EQ(expectedOutput2, output2);
 
 }
 
@@ -38,74 +55,98 @@ TEST(SingleCommandTest, WrongCommand){
     
     singleCommand* s3 = new singleCommand("wrong");
     s3->Parse();
+    testing::internal::CaptureStdout();
     s3->runCommand();
-
+    string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ("", output);
 }
 
 
 TEST(MultipleCommandTest, Mixed1) {
     
-    multipleCommand* m1 = new multipleCommand("echo hello || ls ; echo "my && name" && echo "is || mel" && ls");
+    multipleCommand* m1 = new multipleCommand("echo hello || ls ; echo \"my && name\" && echo \"is || mel\" && ls");
     m1->Parse();
+    testing::internal::CaptureStdout();
     m1->runCommand();
-    EXPECT_EQ();
-    
+    string output = testing::internal::GetCapturedStdout();
+    string expectedOutput = "hello\nmy && name\nis || mel\nbin\nCMakeCache.txt\nCMakeFiles\ncmake_install.cmake\nCMakeLists.txt\ngoogletest\nimages\nintegration_tests\nlib\nMakefile\nREADME.md\nrshell\nsrc\ntest\nunit_tests\n";
+    EXPECT_EQ(expectedOutput, output);
 }
 
 TEST(MultipleCommandTest, Mixed2) {
     
-    multipleCommand* m2 = new multipleCommand("ls -a; echo hello; mkdir test");
+    multipleCommand* m2 = new multipleCommand("ls -a; echo hello; mkdir newDir; ls");
     m2->Parse();
+     testing::internal::CaptureStdout();
     m2->runCommand();
-    EXPECT_EQ();
-        
+    string output = testing::internal::GetCapturedStdout();
+    string expectedOutput = ".\n..\nbin\nCMakeCache.txt\nCMakeFiles\ncmake_install.cmake\nCMakeLists.txt\n.git\n.gitignore\n.gitmodules\ngoogletest\nimages\nintegration_tests\nlib\nMakefile\nREADME.md\nrshell\nsrc\ntest\nunit_tests\nhello\nbin\nCMakeCache.txt\nCMakeFiles\ncmake_install.cmake\nCMakeLists.txt\ngoogletest\nimages\nintegration_tests\nlib\nMakefile\nnewDir\nREADME.md\nrshell\nsrc\ntest\nunit_tests\n";
+    EXPECT_EQ(expectedOutput, output);
+    multipleCommand* m6 = new multipleCommand("rm -rf newDir");
+    m6->Parse();
+    m6->runCommand();    
 }
 
 TEST(MultipleCommandTest, Mixed3) {
     
-    multipleCommand* m3 = new multipleCommand("ls -a; echo hello && mkdir test || echo world; git status");
+    multipleCommand* m3 = new multipleCommand("echo hello || ls -1 ; echo hi || echo world || git status");
     m3->Parse();
+    testing::internal::CaptureStdout();
     m3->runCommand();
-    EXPECT_EQ();
+    string output = testing::internal::GetCapturedStdout();
+    string expectedOutput = "hello\nhi\n";
+    EXPECT_EQ(expectedOutput, output);
 }
 
 
 TEST(MultipleCommandTest, Mixed4){
     
-    multipleCommand* m4 = new multipleCommand(echo "hello" ; ls || echo "don't print");
+    multipleCommand* m4 = new multipleCommand("echo \"hello\" ; ls || echo \"don't print\"");
     m4->Parse();
-    m5->runCommand();
-    EXPECT_EQ();
+    testing::internal::CaptureStdout();
+    m4->runCommand();
+    string output = testing::internal::GetCapturedStdout();
+     string expectedOutput = "hello ; ls\n";
+    EXPECT_EQ(expectedOutput, output);
 
 }
 
 TEST(CommentedCommandTest, Comment1){
     singleCommand* s1 = new singleCommand("echo love cs # wont print");
     s1->Parse();
+    testing::internal::CaptureStdout();
     s1->runCommand();
-    
+    string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "love cs\n");
 }
 
 
 TEST(CommentedCommandTest, Comment2){
-    singleCommand* s1 = new singleCommand("ls -a #ls");
+    singleCommand* s1 = new singleCommand("ls # -a");
     s1->Parse();
+    testing::internal::CaptureStdout();
     s1->runCommand();
-
+    string output = testing::internal::GetCapturedStdout();
+    string expectedOutput = "bin\nCMakeCache.txt\nCMakeFiles\ncmake_install.cmake\nCMakeLists.txt\ngoogletest\nimages\nintegration_tests\nlib\nMakefile\nREADME.md\nrshell\nsrc\ntest\nunit_tests\n";
+    EXPECT_EQ(output, expectedOutput);
 }
 
 TEST(CommentedCommandTest, Comment3){
     multipleCommand* m1 = new multipleCommand("echo hello #wont print ; echo hi # dont print");
     m1->Parse();
+    testing::internal::CaptureStdout();
     m1->runCommand();
-
+    string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ("hello\n", output);
 }
 
 TEST(CommentedCommandTest, Comment4){
-    multipleCommand* m1 = new multipleCommand("echo hello  ; echo hi # dont print ; echo this shouldnt print");
+    multipleCommand* m1 = new multipleCommand("echo hello ; echo hi # dont print ; echo this shouldnt print");
     m1->Parse();
+    testing::internal::CaptureStdout();
     m1->runCommand();
-
+    string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ("hello\nhi\n", output);
 
 }
 
@@ -113,49 +154,27 @@ TEST(CommentedCommandTest, Comment4){
 TEST(CommentedCommandTest, Comment5){
     multipleCommand* m1 = new multipleCommand( "#echo hello  ; echo hi dont print");
     m1->Parse();
+    testing::internal::CaptureStdout();
     m1->runCommand();
-
-
+    string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ("", output);
 }
 
-
-TEST(ExitCommandTest, Exit1){
-    singleCommand* s1 = new singleCommand("exit");
-    s1->Parse();
-    s1->runCommand();
-    EXPECT_EQ();
-    
-}
-TEST(ExitCommandTest, Exit2){
-    multipleCommand* m1 = new multipleCommand( "echo hello  ; exit");
-    m1->Parse();
-    m1->runCommand();
-    EXPECT_EQ();
-}
 TEST(ExitCommandTest, Exit3){
     multipleCommand* m1 = new multipleCommand( "echo hello || exit");
     m1->Parse();
+    testing::internal::CaptureStdout();
     m1->runCommand();
-    EXPECT_EQ();
-}
-TEST(ExitCommandTest, Exit4){
-    multipleCommand* m1 = new multipleCommand( "echo hello && exit");
-    m1->Parse();
-    m1->runCommand();
-    EXPECT_EQ();
-}
-TEST(ExitCommandTest, Exit5){
-    multipleCommand* m1 = new multipleCommand( "wrong command || exit");
-    m1->Parse();
-    m1->runCommand();
-    EXPECT_EQ();
+    string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ("hello\n", output);
 }
 
+//extensive Exit testing will be done in integration because the exit here exits the google test stopping multiple exit functions
+//
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
-
 
 
 
