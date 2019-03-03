93e307d72485fa089b2554b9235ed6a53f53ff7d
mel-aguilar <44855284+mel-aguilar@users.noreply.github.com>
2019-03-03 14:06:17 -0800

    Create precedence_tests.sh

---
+++ src/precedence_tests.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# tests primarily for the precedence being respected
+
+./rshell << EOF
+echo 1  && echo "world" ; (echo hi && echo "world")
+echo hi && echo "world" || (echo hi && echo "world")
+echo hi && echo "world" && (echo hi && echo "world")
+echo hi && ls # (echo hi && echo "world")
+(echo 1 && echo 2) && (echo 3 && echo 4) && (echo 5 && echo 6)
+(echo 1 || echo 2) && echo 3
+(echo 1 || echo 2) && echo 3
+(echo 1 || (echo 2 && echo 3))
+(echo 1 || (echo 2 && echo 3) && (echo 4 && echo 5))
+(echo 1 && (echo 2 && echo 3) || (echo 4 ; echo 5))
+(echo 1 ; (echo 2 && echo 3)) && echo 4
+(dog && (echo 3 && echo 5))
+(echo hello && echo hi
+echo "(hello there)" && (echo 2 && echo 4)
+(exit && echo 1) && echo 2;
+(exit && echo 1) || echo 2;
+(exit && echo 1) ; echo 2;
+echo 1 && (exit && echo 2 && echo 3) && echo 4
+echo 1 ; (echo 2 ; exit || echo 3) && echo 5
+echo 1 ; (echo 3 && echo 4 ; exit) || echo 6
+exit
+EOF
