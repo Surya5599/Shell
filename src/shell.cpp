9865ec615fe4e60fe291bbc4dad520814aa98ee0
mel-aguilar <44855284+mel-aguilar@users.noreply.github.com>
2019-03-03 14:20:53 -0800

    Update shell.cpp

--- src/shell.cpp
+++ src/shell.cpp
@@ -7,17 +7,19 @@
 #include "Command.h"
 #include "singleCommand.h"
 #include "multipleCommand.h"
+#include "pareCommands.h"
+
 using namespace std;
 
 string printPrompt(){
     string input;
     cout << "$ ";
     getline(cin, input);
-
     return input;
 } 
 
 void singleExecute(string input) {
+    //cout << "in single" << endl;
     singleCommand* sc1 = new singleCommand(input);
     sc1->Parse();
     sc1->runCommand();
@@ -25,62 +27,116 @@ void singleExecute(string input) {
 }
 
 void multipleExecute(string input){
+    //cout << "in multiple" << endl;
     multipleCommand* mc1 = new multipleCommand(input);
     mc1->Parse();
     mc1->runCommand();
     return;
 }
 
+void parenExecute(string input) {
+    //cout << "in parenthesis" << endl;
+    pareCommands* pc1 = new pareCommands(input);
+    pc1->Parse();
+    pc1->runCommand();
+    return;
+}
+
 bool checkIfSingle(string data){
-        int firstPos;
-        int secPos;
+        int firstPos = 0;
+        int secPos = 0;
         int connCount = 0;
-        int conPos;
+        int conPos = 0;
+        int connInQuotes = 0;
         for(unsigned i = 0; i < data.size(); ++i) {
             if(data[i] == '\"'){
                 firstPos = i;
                 break;
             }
         }
-        for(unsigned j = firstPos; j < data.size(); ++j) {
+        for(unsigned j = firstPos + 1; j < data.size(); ++j) {
                 if(data[j] == '\"') {
                     secPos = j;
+                    break;
                 }
         }
         for(unsigned i = 0; i < data.size(); ++i) {
                 if(data[i] == ';'){
                     conPos = i;
                     ++connCount;
+                    if(conPos > firstPos && conPos < secPos){
+                        connInQuotes++;
+                    }
                 }
                 if(data[i] == '|' && data[i+1] == '|'){
                     conPos = i;
                     ++connCount;
+                    if(conPos > firstPos && conPos < secPos){
+                        connInQuotes++;
+                    }
                 }
                 if(data[i] == '&' && data[i+1] == '&') {
                     conPos = i;
                     ++connCount;
+                    if(conPos > firstPos && conPos < secPos){
+                        connInQuotes++;
+                    }
                 }
         }
-        if(connCount > 1){
-            return false;
-        }
-        if(connCount == 1){
-            if(conPos > firstPos && conPos < secPos){
-                return true;
-            }
+        if(connInQuotes == connCount){
+            return true;
         }
         return false;
 }
 
-int main(){
-    string userInput = "";
+bool checkParethesis(string data) {
+        bool parenExist = false;
+        int firstPos = -1;
+        int secPos = -1;
+        for(unsigned i = 0; i < data.size(); ++i){
+            if(data[i] == '('){
+                parenExist = true;
+            }
+            if(data[i] == ')'){
+                parenExist = true;
+            }
+        }
+        return parenExist;
+        // int connCount = 0;
+        // int conPos = 0;
+        // bool inPare = false;
+        // for(unsigned i = 0; i < data.size(); ++i) {
+        //         if(data[i] == ';'){
+        //             conPos = i;
+        //             ++connCount;
+        //             if(conPos > firstPos && conPos < secPos){
+        //                 inPare = true;
+        //             }
+        //         }
+        //         if(data[i] == '|' && data[i+1] == '|'){
+        //             conPos = i;
+        //             ++connCount;
+        //             if(conPos > firstPos && conPos < secPos){
+        //                 inPare = true;
+        //             }
+        //         }
+        //         if(data[i] == '&' && data[i+1] == '&') {
+        //             conPos = i;
+        //             ++connCount;
+        //             if(conPos > firstPos && conPos < secPos){
+        //                 inPare = true;
+        //             }
+        //         }
+        // }
+        // if(inPare == true && (connCount > 1)){
+        //     return true;
+        // }
+        // return false;
+}
+
+bool checkIfMultiple(string userInput){
     bool multiple = false;
-    
-    
-    
-    while(userInput != "exit"){
-        userInput = printPrompt();
-        for(unsigned i = 0; i < userInput.size(); ++i) {
+    for(unsigned i = 0; i < userInput.size(); ++i) {
             if(userInput[i] == ';'){
                 multiple = true;
             }
@@ -91,11 +147,18 @@ int main(){
                     multiple = true;
             }
         }
-        
-        if(multiple == false){
-                singleExecute(userInput);
+    return multiple;
+}
+
+int main(){
+    string userInput = "";
+    while(userInput != "exit"){
+        userInput = printPrompt();
+        if (checkParethesis(userInput) == true){
+                parenExecute(userInput);
+            
         }
-        else if(checkIfSingle(userInput) == true){
+        else if(checkIfSingle(userInput) == true || checkIfMultiple(userInput) == false){
             singleExecute(userInput);
         }
         else{
