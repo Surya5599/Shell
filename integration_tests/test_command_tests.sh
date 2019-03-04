660671ca946ab1b15855372c06e06ee11003fdb7
mel-aguilar <44855284+mel-aguilar@users.noreply.github.com>
2019-03-03 16:23:10 -0800

    Update test_command_tests.sh

--- integration_tests/test_command_tests.sh
+++ integration_tests/test_command_tests.sh
@@ -3,18 +3,18 @@
 cd ..
 
 ./a.out << EOF
-test -e shell.cpp && echo "shell is a file/dir"
-test -d pareCommands.h || echo "pareCommands is not a directory"
-test -d ../assignment3 && echo "is a dir"
-test -f Command.h && echo "is a file"
-test -d shell.cpp || echo is not a dir
-test -f multipleCommand.cpp && test -f multipleCommand.h && echo "is a file"
-test -d multipleCommand.cpp || test -d multipleCommand.h || test -d ../assignment3 && echo "is a directoy"
-test -e shell.cpp && echo "shell is a file/dir" && test -d shell.cpp && echo "shell is a dir" || test -f shell.cpp && echo "shell is a file"
-test -e ../assignment3 && echo "is a file/dir" && test -d ../assignment3 && echo "assignment3 is a dir" || test -f ../assignment3 && echo "is a file"
-test -e mel.cpp && echo "is a file/dir" || echo "is not a file/dir"
-test -e Command.h && test -e multipleCommand.h && test -e multipleCommand.cpp && test -e singleCommand.h && test -e singleCommand.cpp && test -e ../assignment2 && echo "are files/directories" && test -f Command.h && test -f multipleCommand.h && test -f multipleCommand.cpp && test -f singleCommand.h && test -f singleCommand.cpp && echo "are files"
-test -e Command.h && test -e multipleCommand.h && test -e multipleCommand.cpp && test -e singleCommand.h && test -e singleCommand.cpp && test -e ../assignment2 && echo "are files/directories" && test -d ../assignment2 && echo "is directory"
-test -e singleCommand.h && echo "is file/directory" || echo "is not file/directory"
+test -e src/shell.cpp && echo "shell.cpp is a file/dir"
+test -d src/pareCommands.h && echo "pareCommands.h is a directory" || echo "pareCommands.h is not a directory"
+test -d src && echo "is a dir"
+test -f src/Command.h && echo "is a file"
+test -d src/shell.cpp || echo "shell.cpp is not a dir"
+test -f src/multipleCommand.cpp && test -f src/multipleCommand.h && echo "multipleCommand.cpp and multipleCommand.h are files"
+test -d src/multipleCommand.cpp || test -d src/multipleCommand.h || test -d integration_tests && echo "integration_tests is a directoy"
+test -e src/shell.cpp && echo "shell.cpp is a file/dir" && test -d src/shell.cpp && echo "shell/cpp is a dir" || test -f src/shell.cpp && echo "shell.cpp is a file"
+test -e unit_tests && echo "is a file/dir" && test -d src && echo "src is a dir" || test -f integration_tests && echo "is a file"
+test -e src/mel.cpp && echo "mel.cpp is a file/dir" || echo "mel.cpp is not a file/dir"
+test -e src/Command.h && test -e src/multipleCommand.h && test -e src/multipleCommand.cpp && test -e src/singleCommand.h && test -e src/singleCommand.cpp && test -e src && echo "are files/directories" && test -f src/Command.h && test -f src/multipleCommand.h && test -f src/multipleCommand.cpp && test -f src/singleCommand.h && test -f src/singleCommand.cpp && echo "are files"
+test -e src/Command.h && test -e src/multipleCommand.h && test -e src/multipleCommand.cpp && test -e src/singleCommand.h && test -e src/singleCommand.cpp && test -e unit_tests && echo "are files/directories" && test -d unit_tests && echo "unit_tests is directory"
+test -e src/singleCommand.h && echo "singleCommand.h is file/directory" || echo "singleCommand.h is not file/directory"
 exit
 EOF
